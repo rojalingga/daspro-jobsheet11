f902911f9f4c21edd9d349b3f2cdc419c04c102c
rojalingga <rojalingga2006@gmail.com>
2024-11-22 10:07:37 +0700

    varargs

---
+++ PengunjungCafe_20.java
@@ -0,0 +1,15 @@
+package Pertemuan13;
+
+public class PengunjungCafe_20 {
+
+    public static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+    }
+}
