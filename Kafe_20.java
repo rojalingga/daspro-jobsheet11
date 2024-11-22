f00a5810e8dd06d7e651ed1494ffafae55edfdf3
rojalingga <rojalingga2006@gmail.com>
2024-11-22 09:32:06 +0700

    add params & program  kodePromo

--- Kafe_20.java
+++ Kafe_20.java
@@ -2,13 +2,21 @@ package Pertemuan13;
 
 public class Kafe_20 {
 
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
-        
+    
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
-        
+    
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Berikan diskon 50%!");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Berikan diskon 30%!");
+        } else {
+            System.out.println("Kode promo tidak valid.");
+        }
+    
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam – Rp 15,000");
         System.out.println("2. Cappuccino – Rp 20,000");
@@ -21,7 +29,7 @@ public class Kafe_20 {
     }
     
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON50");
     }
     
 }
