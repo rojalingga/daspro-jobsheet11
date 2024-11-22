6479fa63045e7d9ebaecc45b9f28449f512d1632
rojalingga <rojalingga2006@gmail.com>
2024-11-22 09:30:13 +0700

    Add params namaPelanggan dan member

--- Kafe_20.java
+++ Kafe_20.java
@@ -2,7 +2,13 @@ package Pertemuan13;
 
 public class Kafe_20 {
 
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+        
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam – Rp 15,000");
         System.out.println("2. Cappuccino – Rp 20,000");
@@ -13,9 +19,9 @@ public class Kafe_20 {
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
-
+    
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
     
 }
