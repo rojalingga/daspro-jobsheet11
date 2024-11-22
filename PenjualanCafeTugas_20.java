07f465130ef13fa85d1c2a3370a1bc2b50656ce2
rojalingga <rojalingga2006@gmail.com>
2024-11-22 20:05:20 +0700

    Tugas 2 Cafe

---
+++ PenjualanCafeTugas_20.java
@@ -0,0 +1,71 @@
+package Pertemuan13;
+import java.util.Scanner;
+
+public class PenjualanCafeTugas_20 {
+    static int[][] jmlPenjualan = new int[5][7];
+    static String[] namaMenu = {"Kopi", "Teh", "Es Degan", "Roti Bakar", "Gorengan"};
+
+    public static void inputDataPenjualan() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < jmlPenjualan.length; i++) {
+            System.out.println("Masukkan Jumlah Penjualan untuk " +namaMenu[i]+ " (minggu 1 - 7) : ");
+            for (int j = 0; j < jmlPenjualan[i].length; j++) {
+                System.out.print("Minggu ke-" + (j+1) + " : ");
+                jmlPenjualan[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void tampilkanData() {
+        System.out.println("Data Jumlah Penjualan : ");
+        System.out.println("Menu\t\tH1\tH2\tH3\tH4\tH5\tH6\tH7");
+        for (int i = 0; i < jmlPenjualan.length; i++) {
+            System.out.print(namaMenu[i] + "\t");
+            if (namaMenu[i].length() < 8)
+                System.out.print("\t");
+            for (int j = 0; j < jmlPenjualan[i].length; j++) {
+                System.out.print(jmlPenjualan[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void penjualanTertinggi() {
+        int maxTotal = 0;
+        String maxMenu = "";
+
+        for (int i = 0; i < jmlPenjualan.length; i++) {
+            int total = 0;
+            for (int j = 0; j < jmlPenjualan[i].length; j++) {
+                total += jmlPenjualan[i][j];
+            }
+            if (total > maxTotal) {
+                maxTotal = total;
+                maxMenu = namaMenu[i];
+            }
+        }
+
+        System.out.println("Menu dengan penjualan tertinggi: " + maxMenu);
+        System.out.println("Total penjualan: " + maxTotal);
+        System.out.println();
+    }
+
+    public static void rataRataPerMenu() {
+        for (int i = 0; i < jmlPenjualan.length; i++) {
+            double total = 0;
+            for (int j = 0; j < jmlPenjualan[i].length; j++) {
+                total += jmlPenjualan[i][j];
+            }
+            double rata = total / jmlPenjualan[i].length;
+            System.out.printf("Rata-rata penjualan menu %s dalam %d minggu: %.2f%n", namaMenu[i], jmlPenjualan[i].length, rata);
+        }
+    }
+
+    public static void main(String[] args) {
+        inputDataPenjualan();
+        tampilkanData();
+        penjualanTertinggi();
+        rataRataPerMenu();
+    }
+}
